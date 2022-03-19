a4ef14df6bc40f672510170a85ee73ada296fdfe
dimanoit <dimanoit@gmail.com>
2022-03-19 16:52:54 +0200

    add method to interface

--- Soulgram.File.Manager/Interfaces/IFileManager.cs
+++ Soulgram.File.Manager/Interfaces/IFileManager.cs
@@ -1,7 +1,10 @@
+using Azure.Storage.Blobs.Models;
+
 namespace Soulgram.File.Manager;
 
 public interface IFileManager
 {
-    public Task<string> UploadFileAsync(FileInfo file, string userId);
-    public Task<IEnumerable<string>> UploadFilesAndGetIds(IEnumerable<FileInfo> files, string userId);
+     Task<string> UploadFileAsync(FileInfo file, string userId);
+     Task<IEnumerable<string>> UploadFilesAndGetIds(IEnumerable<FileInfo> files, string userId); 
+     Task<string> UploadFileAsync(FileInfo file, string userId, BlobUploadOptions options);
 }
